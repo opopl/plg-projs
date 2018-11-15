9da8575f66c3bce4420d92fe688e6a4e21bf1226
op <op@localhost>
2018-11-15 17:21:45 +0200

    u

--- php/index.php
+++ php/index.php
@@ -1,19 +1,30 @@
 <?php
 	$dir = getenv('PROJSDIR');
 
-	if ($handle = opendir($dir)) {
-	    echo "Directory handle: $handle\n";
-	    echo "Entries:\n";
+	if (isset($_GET['action'])) {
+		 $action = $_GET['action'];
 
-			$i=0;
+		 if ($action == 'listfiles') {
+				listfiles($dir);
+		 }
+	}
+
+	function listfiles($dir)
+	{
+		if ($handle = opendir($dir)) {
+		    echo "Directory handle: $handle\n";
+		    echo "Entries:\n";
 	
-	    /* This is the correct way to loop over the directory. */
-	    while (false !== ($entry = readdir($handle))) {
-				 	echo "<br>$entry\n";
-					$i++;
-					if ($i==100) break;
-	    }
-	
-	    closedir($handle);
+				$i=0;
+		
+		    /* This is the correct way to loop over the directory. */
+		    while (false !== ($entry = readdir($handle))) {
+					 	echo "<br>$entry\n";
+						$i++;
+						if ($i==100) break;
+		    }
+		
+		    closedir($handle);
+		}
 	}
 ?>
