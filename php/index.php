00e2cdd2e08af29298edef56322a91ee311e3cc5
op <op@localhost>
2018-11-15 17:09:50 +0200

    u

--- php/index.php
+++ php/index.php
@@ -1,3 +1,19 @@
 <?php
+	$dir = getenv('PROJSDIR');
 
+	if ($handle = opendir($dir)) {
+	    echo "Directory handle: $handle\n";
+	    echo "Entries:\n";
+
+			$i=0;
+	
+	    /* This is the correct way to loop over the directory. */
+	    while (false !== ($entry = readdir($handle))) {
+				 	echo "<br>$entry\n";
+					$i++;
+					if ($i==100) break;
+	    }
+	
+	    closedir($handle);
+	}
 ?>
