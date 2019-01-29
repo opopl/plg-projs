7aadd0bef570d580c74b94ede7055fc5053f0496
op <op@localhost>
2019-01-29 11:52:59 +0200

    u

--- scripts/db_fill.py
+++ scripts/db_fill.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--proj", help="select_project",default="")
 parser.add_argument("-r", "--root",help="root",default="")
 parser.add_argument("--rootid", help="rootid",default="")
+parser.add_argument("--db_file", help="db_file",default="")
 
 args=parser.parse_args()
 
@@ -24,11 +25,14 @@ if args.rootid:
     rootid=args.rootid
 
 db_file = os.path.join(root,'projs.sqlite')
+if args.db_file:
+    db_file=args.db_file
+
 proj = 'book_css_meier'
 
 def logfun(e):
     print(e)
 
-#db.drop_tables(db_file)
-#db.create_tables(db_file)
-##db.fill_from_files(db_file,root,rootid,proj,logfun)
+db.drop_tables(db_file)
+db.create_tables(db_file)
+db.fill_from_files(db_file,root,rootid,proj,logfun)
