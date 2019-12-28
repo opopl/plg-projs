d50a40964a5c21c35dbfb3a7a27cc18512fdc773
op <op@localhost>
2019-12-29 01:54:55 +0200

    u

---
+++ scripts/gui/proj_toolbar.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl 
+
+use strict;
+use warnings;
+use utf8;
+
+use Data::Dumper qw(Dumper);
+
+package main;
+
+use FindBin qw($Bin);
+use lib "$Bin/../../perl/lib";
+
+use base qw( Plg::Projs::Dialog::ProjToolbar );
+
+__PACKAGE__->new->run;
+
