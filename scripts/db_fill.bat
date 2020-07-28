f822f09695286808fcc10195920935f109741b25
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-07-28 12:10:39 +0300

    u

--- scripts/db_fill.bat
+++ scripts/db_fill.bat
@@ -1,5 +1,7 @@
 
 @echo off
 
+call vars_python_2.bat
+
 set Bin=%~dp0
 python %Bin%\db_fill.py %*
--- scripts/db_fill.py
+++ scripts/db_fill.py
@@ -14,6 +14,7 @@ parser.add_argument("-r", "--root",help="root",default="")
 parser.add_argument("--rootid", help="rootid",default="")
 parser.add_argument("--db_file", help="db_file",default="")
 parser.add_argument("-a", "--all", help="fill all projects",default=1)
+parser.add_argument("-l","--list", help="list of projects",default="")
 
 args = parser.parse_args()
 
@@ -21,28 +22,36 @@ if len(sys.argv) == 1:
   parser.print_help()
   sys.exit()
 
-dirname=os.path.dirname(os.path.realpath(__file__))
-pylib=os.path.join(dirname,'..','python','lib')
+dirname = os.path.dirname(os.path.realpath(__file__))
+pylib   = os.path.join(dirname,'..','python','lib')
+
 sys.path.append(pylib)
 import plg.projs.db as db
 
 root=os.path.abspath("")
 if args.root:
-    root=args.root
+    root = args.root
 
 rootid=""
 if args.rootid:
-    rootid=args.rootid
+    rootid = args.rootid
 
 db_file = os.path.join(root,'projs.sqlite')
 if args.db_file:
-    db_file=args.db_file
+    db_file = args.db_file
 
 proj = ''
 
 def logfun(e):
     print(e)
 
+if args.list:
+    list = args.list
+    projs = list.split(",")
+    for proj in projs:
+        db.fill_from_files(db_file, root, rootid, proj, logfun)
+    return
+
 db.drop_tables(db_file)
 db.create_tables(db_file)
-db.fill_from_files(db_file,root,rootid,proj,logfun)
+db.fill_from_files(db_file, root, rootid, proj, logfun)
