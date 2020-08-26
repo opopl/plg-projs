62194a3ecc76570ef81d0e314bd40abf1f940b6c
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-08-26 09:48:51 +0300

    u

--- perl/lib/Plg/Projs/Scripts/MkInd.pm
+++ perl/lib/Plg/Projs/Scripts/MkInd.pm
@@ -1,9 +1,62 @@
 
-
 package Plg::Projs::Scripts::MkInd;
 
 use strict;
 use warnings;
+
+use Plg::Projs::Build::PdfLatex;
+
+use FindBin qw($Bin $Script);
+use Cwd;
+use Data::Dumper qw(Dumper);
+
+use File::stat;
+use File::Path qw(rmtree);
+
+sub new
+{
+	my ($class, %opts) = @_;
+	my $self = bless (\%opts, ref ($class) || $class);
+
+	$self->init if $self->can('init');
+
+	return $self;
+}
+
+sub init {
+    my $self = shift;
+
+    unless (@ARGV) {
+        print qq{
+            LOCATION:
+                $0
+            USAGE:
+                $Script PROJ
+        } . "\n";
+        exit 1;
+    }
+
+    my $proj = shift @ARGV;
+    my $root = getcwd();
+
+    my $blx = Plg::Projs::Build::PdfLatex->new( 
+        skip_get_opt => 1,
+        proj         => $proj,
+        root         => $root,
+    );
+
+    my $h = {
+        proj => $proj,
+        root => $root,
+        blx  => $blx,
+    };
+        
+    my @k = keys %$h;
+
+    for(@k){ $self->{$_} = $h->{$_} unless defined $self->{$_}; }
+
+    return $self;
+}
 
 1;
  
