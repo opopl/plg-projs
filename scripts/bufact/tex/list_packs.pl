f4f62bdb2e720b46789a495f4f83275e84b6341b
op <op@localhost>
2020-10-06 15:20:41 +0300

    u

--- scripts/bufact/tex/list_packs.pl
+++ scripts/bufact/tex/list_packs.pl
@@ -6,10 +6,39 @@ use utf8;
 
 use Data::Dumper qw(Dumper);
 use File::Slurp::Unicode;
+use Data::Dumper qw(Dumper);
+
+use Base::List qw(uniq);
 
 my $file = shift @ARGV;
 
-my @lines=read_file $file;
+my @lines = read_file $file;
+
+my @pack_list;
+my %pack_opts;
+
 for (@lines) {
-	chomp;
+    chomp;
+    m/^\\usepackage(?:|\[(?<opts>.*)\])\{(?<packs>.*)\}/ && do {
+        my @p = split "," => $+{'packs'};
+        push @pack_list, @p;
+
+        if (my $o = $+{opts}) {
+            my @opts = split "," => $o;
+
+            foreach my $pack (@p) {
+                $pack_opts{$pack} ||= [];
+
+                push @{$pack_opts{$pack}}, $o; 
+            }
+        }
+
+        next;
+    };
 }
+
+@pack_list = uniq(\@pack_list);
+
+print Dumper(\@pack_list) . "\n";
+print Dumper(\%pack_opts) . "\n";
+
