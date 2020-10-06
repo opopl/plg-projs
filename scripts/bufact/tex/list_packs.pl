6e7d500202806906026a6d9f3ba3f1086518baff
op <op@localhost>
2020-10-06 15:52:33 +0300

    u

--- scripts/bufact/tex/list_packs.pl
+++ scripts/bufact/tex/list_packs.pl
@@ -17,28 +17,55 @@ my @lines = read_file $file;
 my @pack_list;
 my %pack_opts;
 
-for (@lines) {
+my $is_pack;
+my @ot;
+
+sub pack_add {
+    my ($p,$o) = @_;
+
+    my @p = split "," => $p;
+    push @pack_list, @p;
+
+    return unless $o;
+
+    my @opts = split "," => $o;
+
+    foreach my $pack (@p) {
+        $pack_opts{$pack} ||= [];
+
+        push @{$pack_opts{$pack}}, $o; 
+    }
+}
+
+while (@lines) {
+    local $_ = shift @lines;
     chomp;
+
     m/^\\usepackage(?:|\[(?<opts>.*)\])\{(?<packs>.*)\}/ && do {
-        my @p = split "," => $+{'packs'};
-        push @pack_list, @p;
-
-        if (my $o = $+{opts}) {
-            my @opts = split "," => $o;
-
-            foreach my $pack (@p) {
-                $pack_opts{$pack} ||= [];
-
-                push @{$pack_opts{$pack}}, $o; 
-            }
-        }
+        pack_add($+{qw(packs opts)});
 
         next;
     };
+
+    m/^\\usepackage\[(?<opts>[^]]*)$/ && do {
+        push @ot, $+{opts};
+        $is_pack = 1;
+        next;
+    };
+
+    if ($is_pack) {
+        m/^\s*\]\{(?<packs>.*)\}$/ && do {
+            $is_pack = 0;
+            next;
+        };
+
+        push @ot, $_;
+    }
 }
 
 @pack_list = uniq(\@pack_list);
 
 print Dumper(\@pack_list) . "\n";
 print Dumper(\%pack_opts) . "\n";
+print Dumper(\@ot) . "\n";
 
