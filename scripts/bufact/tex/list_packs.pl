bb452b48b21bd41fb0ca1bb757dcb5b73aab8ecd
op <op@localhost>
2020-10-06 16:04:02 +0300

    u

--- scripts/bufact/tex/list_packs.pl
+++ scripts/bufact/tex/list_packs.pl
@@ -28,7 +28,7 @@ sub pack_add {
 
     return unless $o;
 
-    my @opts = split "," => $o;
+    my @opts = opts_split($o);
 
     foreach my $pack (@p) {
         $pack_opts{$pack} ||= [];
@@ -37,35 +37,59 @@ sub pack_add {
     }
 }
 
+sub opts_split {
+    my ($o) = @_;
+
+    return @$o if ref $o eq 'ARRAY';
+
+    my @opts = map { 
+        s/\s*$//g; 
+        s/^\s*//g; 
+        s/[%]*$//g; 
+        length ? $_ : () 
+    } split "," => $o;
+
+    return @opts;
+}
+
 while (@lines) {
     local $_ = shift @lines;
     chomp;
 
+    next if /^\s*%/;
+
     m/^\\usepackage(?:|\[(?<opts>.*)\])\{(?<packs>.*)\}/ && do {
-        pack_add($+{qw(packs opts)});
+        pack_add(@+{qw(packs opts)});
 
         next;
     };
 
+    # start of package
     m/^\\usepackage\[(?<opts>[^]]*)$/ && do {
-        push @ot, $+{opts};
+        push @ot, opts_split($+{opts});
         $is_pack = 1;
         next;
     };
 
     if ($is_pack) {
+
+        # end of package
         m/^\s*\]\{(?<packs>.*)\}$/ && do {
             $is_pack = 0;
+            pack_add(@+{qw(packs)},\@ot);
             next;
         };
 
-        push @ot, $_;
+        push @ot, opts_split($_);
     }
 }
 
 @pack_list = uniq(\@pack_list);
 
-print Dumper(\@pack_list) . "\n";
-print Dumper(\%pack_opts) . "\n";
-print Dumper(\@ot) . "\n";
+my $h = {
+    pack_list => \@pack_list,
+    pack_opts => \%pack_opts,
+};
+
+print Dumper($h) . "\n";
 
