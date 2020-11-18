72d2581e0e3dc41c07010d32f960b84fd44af933
op <op@localhost>
2020-11-18 10:50:08 +0200

    u

--- perl/lib/Plg/Projs/Scripts/DoTexify.pm
+++ perl/lib/Plg/Projs/Scripts/DoTexify.pm
@@ -16,84 +16,92 @@ use Getopt::Long qw(GetOptions);
 
 sub new
 {
-	my ($class, %opts) = @_;
-	my $self = bless (\%opts, ref ($class) || $class);
+    my ($class, %opts) = @_;
+    my $self = bless (\%opts, ref ($class) || $class);
 
-	$self->init if $self->can('init');
+    $self->init if $self->can('init');
 
-	return $self;
+    return $self;
 }
 
 
 sub init {
-	my ($self) = @_;
-	
-	$self->get_opt;
-	
-	my $h = {
-	};
-		
-	hash_inject($self, $h);
-	return $self;
+    my ($self) = @_;
+    
+    $self->get_opt;
+    
+    my $h = {
+    };
+        
+    hash_inject($self, $h);
+    return $self;
 }
 
       
 sub get_opt {
-	my ($self) = @_;
-	
-	Getopt::Long::Configure(qw(bundling no_getopt_compat no_auto_abbrev no_ignore_case_always));
-	
-	my (@optstr, %opt);
-	@optstr=( 
-		"start|s=s",
-		"end|e=s",
-		"file|f=s",
-	);
-	
-	unless( @ARGV ){ 
-		$self->print_help;
-		exit 0;
-	}else{
-		GetOptions(\%opt,@optstr);
-		$self->{opt} = \%opt;
-	}
+    my ($self) = @_;
+    
+    Getopt::Long::Configure(qw(bundling no_getopt_compat no_auto_abbrev no_ignore_case_always));
+    
+    my (@optstr, %opt);
+    @optstr=( 
+        "start|s=s",
+        "end|e=s",
+        "file|f=s",
+    );
+    
+    unless( @ARGV ){ 
+        $self->print_help;
+        exit 0;
+    }else{
+        GetOptions(\%opt,@optstr);
+        $self->{opt} = \%opt;
+    }
 
-	return $self;	
+    foreach my $k (keys %opt) {
+        $self->{$k} = $opt{$k};
+    }
+
+    return $self;   
 }
 
 sub print_help {
-	my ($self) = @_;
+    my ($self) = @_;
 
-	my $s = qq{
+    my $s = qq{
 
-	USAGE
-		$Script OPTIONS
-	OPTIONS
+    USAGE
+        $Script OPTIONS
+    OPTIONS
+        -f --file FILE
+        -s --start START position (line number)
+        -e --end END position (line number)
 
-	EXAMPLES
-		$Script -f FILE
-		$Script --file FILE
+    EXAMPLES
+        $Script -f FILE
+        $Script --file FILE
 
-		$Script -f FILE -s START -e END
+        $Script -f FILE -s START -e END
 
-	};
+    };
 
-	print $s . "\n";
+    print $s . "\n";
 
-	return $self;	
+    return $self;   
 }
 
-my $file = shift @ARGV;
-my $tex  = read_file $file;
-
-texify(\$tex);
 
 sub run {
-	my ($self) = @_;
+    my ($self) = @_;
 
-	write_file($file,$tex);
+    my $file = $self->{file};
+    my $tex  = read_file $file;
+    
+    texify(\$tex);
 
-	return $self;
+    write_file($file,$tex);
+
+    return $self;
 }
 
 1;
