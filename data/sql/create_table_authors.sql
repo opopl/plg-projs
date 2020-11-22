af44c3d4eec84422cfac8bb5682e2c83364c23bb
op <op@localhost>
2020-11-22 17:31:02 +0200

    u

--- data/sql/create_table_authors.sql
+++ data/sql/create_table_authors.sql
@@ -1,6 +1,9 @@
 
 CREATE TABLE IF NOT EXISTS authors (
-    author_id TEXT,
-    author TEXT
+    author_id TEXT UNIQUE,
+    author TEXT,
+    a_url_img TEXT,
+    a_url_page TEXT,
+    a_info TEXT
 );
 
