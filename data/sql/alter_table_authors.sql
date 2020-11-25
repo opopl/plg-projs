f551e00aefce68b53977d1f9ad35ebdbee1108dc
op <op@localhost>
2020-11-25 13:04:48 +0200

    u

--- data/sql/alter_table_authors.sql
+++ data/sql/alter_table_authors.sql
@@ -1,3 +1,2 @@
 
 alter table authors add column root_id TEXT;
-alter table authors add column author_url TEXT;
--- data/sql/alter_table_projs.sql
+++ data/sql/alter_table_projs.sql
@@ -1,3 +1,4 @@
 
-alter table projs add column author TEXT;
-alter table projs add column id TEXT;
+-- alter table projs add column author TEXT;
+-- alter table projs add column id TEXT;
+-- alter table projs add column author_url TEXT;
