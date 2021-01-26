b134b1fc9b000595302339ee4d457d2ef2e28e28
op <op@localhost>
2021-01-26 12:32:39 +0200

    u

--- python/lib/Base/Scraper/SitePage.py
+++ python/lib/Base/Scraper/SitePage.py
@@ -14,6 +14,8 @@ import Base.Util as util
 import Base.Const as const
 # ----------------------------
 
+from Base.Scraper.Author import Author
+
 class SitePage:
 
   soup        = None
@@ -24,8 +26,51 @@ class SitePage:
     for k, v in args.items():
       setattr(self, k, v)
 
-  def get_author(self,ref={}):
-    return self
-
   def get_date(self,ref={}):
     return self
+
+  def get_author(self,ref={}):
+    site = self.app.site
+
+    sel = ref.get('sel','')
+    auth_sel = util.get( self.app, [ 'sites', site, 'sel', 'author' ] )
+    if not auth_sel:
+     return self
+
+    if type(auth_sel) is dict:
+
+      auth_obj = Author({ 
+        'spage' : self, 
+        'app'   : self.app 
+      })
+
+      d = {}
+
+      d_parse = {}
+      for k in util.qw('url name'):
+        d  = auth_sel.get(k)
+        css  = d.get('css')
+        attr = d.get('attr')
+
+        els = self.soup.select(css)
+  
+        for e in els:
+          auth = None
+    
+          if k == 'url':
+            if e.has_attr(attr):
+              auth_url  = util.url_join(self.app.base_url, e[attr])
+              print(f'[SitePage] found author url: {auth_url}')
+
+              d_parse.update({ 'url' : auth_url })
+          elif k == 'name':
+            auth_bare = e.string
+            if auth_bare:
+              print(f'[SitePage] found author name: {auth_bare}')
+
+              d_parse.update({ 'str' : auth_bare })
+
+      auth_obj.parse(d_parse)
+
+    return self
+
