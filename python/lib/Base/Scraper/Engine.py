0cd4aa4fe4186a8af7971b6ff668568ece98bddc
op <op@localhost>
2021-02-13 14:53:11 +0200

    u

--- python/lib/Base/Scraper/Engine.py
+++ python/lib/Base/Scraper/Engine.py
@@ -1395,7 +1395,6 @@ This script will parse input URL
   def site_extract(self):
     
     hsts = self.hosts
-    import pdb; pdb.set_trace()
     try:
       for pat in hsts.keys():
         for k in pat.split(','):
