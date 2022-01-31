2a7b6173ee6bd834fdb7188e7849ad25637b9312
op <op@localhost>
2022-01-31 15:31:05 +0200

    u

--- python/lib/plg/projs/Prj/Section.py
+++ python/lib/plg/projs/Prj/Section.py
@@ -43,10 +43,97 @@ class Section(
     r = self.__dict__
     return r
 
-#  def _file(self,ref = {}):
-    #if not self.sec:
-      #return 
+  def _file(self,ref = {}):
+    if not self.sec:
+      return 
 
-    #sec_file = os.path.join( self.lts_root, f'{self.proj}.{sec}.tex' )
+    proj = self.proj
+    sec  = self.sec
 
-    #return sec_file
+    runext = 'sh' 
+
+    sfile_a = []
+    if sec == '_main_':
+      sfile_a = [ f'{proj}.tex' ]
+
+    elif sec == '_vim_':
+      sfile_a = [ f'{proj}.vim' ]
+
+    elif sec == '_zlan_':
+      sfile_a = [ f'{proj}.zlan' ]
+
+    elif re.search(r'^_bld\.', sec):
+      
+      target = re.sub(r'^_bld\.(.*)$', r'\1', sec)
+      sfile_a = [ f'{proj}.bld.{target}.yml' ]
+
+    elif re.search(r'^_perl\.', sec):
+
+      sec = re.sub(r'^_perl\.(.*)$', r'\1', sec)
+      sfile_a = [ f'{proj}.{sec}.pl' ]
+
+    elif re.search(r'^_pm\.', sec):
+
+      sec = re.sub(r'^_pm\.(.*)$', r'\1', sec)
+      sfile_a = [ 'perl', 'lib', 'projs', root_id, proj, f'{sec}.pm' ]
+
+    elif sec == '_pl_':
+      sfile_a = [ f'{proj}.pl' ]
+
+    elif sec == '_sql_':
+      sfile_a = [ f'{proj}.sql' ]
+
+    elif sec == '_xml_':
+      sfile_a = [ f'{proj}.xml' ]
+
+    elif sec == '_osecs_':
+      sfile_a = [ f'{proj}.secorder.i.dat']
+
+    elif sec == '_dat_':
+      sfile_a = [ f'{proj}.secs.i.dat' ]
+
+    elif sec == '_ii_include_':
+      sfile_a = [ f'{proj}.ii_include.i.dat' ]
+
+    elif sec == '_ii_exclude_':
+      sfile_a = [ f'{proj}.ii_exclude.i.dat' ]
+
+    elif sec == '_dat_defs_':
+      sfile_a = [ f'{proj}.defs.i.dat' ]
+
+    elif sec == '_dat_files_':
+      sfile_a = [ f'{proj}.files.i.dat' ]
+
+    elif sec == '_dat_files_ext_':
+      sfile_a = [ f'{proj}.files_ext.i.dat' ]
+
+    elif sec == '_dat_citn_':
+      sfile_a = [ f'{proj}.citn.i.dat' ]
+
+    elif sec == '_bib_':
+      sfile_a = [ f'{proj}.refs.bib']
+
+    elif sec == '_tex_jnd_':
+      sfile_a = [ 'builds', proj, 'src', 'jnd.tex' ]
+
+    elif sec == '_join_':
+      sfile_a = [ 'joins', f'{proj}.tex' ]
+
+    elif sec == '_build_pdflatex_':
+      sfile_a = [ f'b_{proj}_pdflatex.{runext}' ]
+
+    elif sec == '_build_perltex_':
+      sfile_a = [ f'b_{proj}_perltex.{runext}' ]
+
+    elif sec == '_build_htlatex_':
+      sfile_a = [ f'b_{proj}_htlatex.{runext}' ]
+
+    elif sec == '_main_htlatex_':
+      sfile_a = [ f'{proj}.main_htlatex.tex' ]
+
+    else:
+      sfile_a = [ f'{proj}.{sec}.tex' ]
+
+    #sec_file = f'{self.proj}.{sec}.tex'
+
+    return sec_file
