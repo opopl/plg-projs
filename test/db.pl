f865e215bdcdcd627fe38e8f26b55d0261e0623d
me <me@localhost>
2022-05-20 13:20:17 +0300

    u

---
+++ test/db.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl 
+
+use strict;
+use warnings;
+use utf8;
+
+use Data::Dumper qw(Dumper);
+use Base::DB qw(
+    dbh_insert_update_hash
+    dbh_base2info
+
+    dbi_connect
+);
+
+use File::Spec::Functions qw(catfile);
+
+my $img_root = $ENV{IMG_ROOT};
+my $repos_git = $ENV{REPOSGIT};
+
+#my $dbfile = catfile($img_root,'img.db');
+my $dbfile = catfile($repos_git,qw( p_pc projs.sqlite ));
+my $dbh = dbi_connect({ dbfile => $dbfile });
+
+my $b2i = { 'tags' => 'tag' };
+
+my (@tags, @author_id);
+push @tags,
+ 'one',
+ 'two',
+ 'four',
+ ;
+push @author_id,
+ 'igor',
+ 'taras',
+ ;
+  
+my $tags = join(",",@tags);
+my $author_id = join(",",@author_id);
+my @files = qw( git.tex git.preamble.tex );
+
+foreach my $file (@files) {
+	my $h = {
+	    tags      => $tags,
+	    parent    => 'bbb',
+	    author_id => $author_id,
+	    file      => $file,
+	};
+	
+	dbh_insert_update_hash({
+	   dbh     => $dbh,
+	   t       => 'projs',
+	   h       => $h,
+	   on_list => [qw( file )],
+	});
+	
+	dbh_base2info({
+	  'dbfile' => $dbfile,
+	  'tbase'  => 'projs',
+	  'bwhere' => { file => $file },
+	  'jcol'   => 'file',
+	  'b2i'    => $b2i,
+	  'bcols'  => [qw( tags author_id )],
+	});
+}
