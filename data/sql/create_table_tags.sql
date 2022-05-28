3c660bbfad9b99b61dc37bd6c286faac912e7b36
me <me@localhost>
2022-05-28 10:40:35 +0300

    u

--- data/sql/create_table_tags.sql
+++
@@ -1,9 +0,0 @@
-
-drop table tags; 
-CREATE TABLE IF NOT EXISTS tags (
-   tag TEXT NOT NULL UNIQUE,
-   -- list of file ids (fid)
-   fids TEXT,
-   rank INTEGER NOT NULL,
-   pids TEXT
-);
--- data/sql/create_table_vimtags.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS vimtags (
-    tag TEXT,
-    tag_id INTEGER PRIMARY KEY,
-    proj TEXT NOT NULL, 
-    rootid TEXT
-);
