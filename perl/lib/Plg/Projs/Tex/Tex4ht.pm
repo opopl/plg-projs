a183dc00e83aa93190db8b1660b7b8443f4f01f2
me <me@localhost>
2022-11-29 14:16:47 +0200

    u

--- perl/lib/Plg/Projs/Tex/Tex4ht.pm
+++ perl/lib/Plg/Projs/Tex/Tex4ht.pm
@@ -14,6 +14,8 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 
 use String::Util qw(trim);
 use Base::List qw(uniq);
+use Deep::Hash::Utils qw( deepvalue );
+use Data::Dumper qw(Dumper);
 
 @ISA     = qw(Exporter);
 @EXPORT  = qw( );
@@ -31,7 +33,7 @@ my @ex_vars_array=qw(
 
 %EXPORT_TAGS = (
 ###export_funcs
-    'funcs' => [qw( 
+    'funcs' => [qw(
         ht_cnf2txt
     )],
     'vars'  => [ @ex_vars_scalar,@ex_vars_array,@ex_vars_hash ]
@@ -50,15 +52,16 @@ sub ht_cnf2txt {
 
     foreach my $x (@$content) {
         local $_ = trim($x);
-        next unless length;
 
-        /^\@\@(?<key>\w+)(|\{(?<path>.*)\})$/ && do { 
+        /^\@\@(?<key>\w+)(|\{(?<path>.*)\})$/ && do {
             my ($key, $path) = @+{qw(key path)};
 
-            my $w = $cnf->{'@@' . $key };
-            next unless $w;
+            my $entry = $cnf->{'@@' . $key };
+            next unless $entry;
 
-            my $sprintf = $w->{'@sprintf'};
+            my $type = $entry->{'@type'} || '';
+            my $sprintf = $entry->{'@sprintf'};
+###sprintf
             if ($sprintf) {
                my ($string, $bare) = @{$sprintf}{qw( string values )};
                my @values;
@@ -79,8 +82,50 @@ sub ht_cnf2txt {
                    }
                    push @txt, sprintf($string, @values);
                }
+               next;
             }
+
+            my @path_a = split('\.', $path);
+            my $wal = deepvalue($entry, @path_a );
+
+###type_css
+            if($type eq 'css'){
+                my @css;
+                if (ref $wal eq 'HASH') {
+                    my @css;
+                    my $indent_css = ' ' x 2;
+                    my $indent_rule = ' ' x 4;
+
+                    while(my($selector, $rule)=each %{$wal}){
+                        next unless ref $rule eq 'HASH';
+                        next unless keys %$rule;
+
+                        my @rule_css;
+                        while(my($k,$v)=each %{$rule}){
+                          push @rule_css, $indent_rule . sprintf('%s : %s;', $k, $v);
+                        }
+                        push @css, $indent_css
+                              . $selector . ' : {' . "\n"
+                              . join("\n" => @rule_css) . "\n"
+                              . $indent_css . '}';
+                    }
+                    push @txt, sprintf('\Css{%s}',"\n" . join("\n" => @css) . "\n") if @css;
+                }
+                next;
+            }
+
+            unless(ref $wal){
+                push @txt, $wal;
+            }elsif (ref $wal eq 'ARRAY') {
+                foreach my $ww (@$wal) {
+                    push @txt, $ww;
+                }
+            }
+
+            next;
         };
+
+        push @txt, $_;
     }
     $DB::single = 1;
 
@@ -89,5 +134,5 @@ sub ht_cnf2txt {
 }
 
 1;
- 
+
 
